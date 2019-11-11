c5c16197ab35b37626d320d6c7dc62750f8f85ff
Dinesh Sharma <Dinesh.Sharma@ac3.com.au>
2019-11-11 13:39:36 +1300

    removed files

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "ami" {
   default = "ami-08a74056dfd30c986"
 }
 variable "subnet_id" {
-  default = "subnet-63ee101"
+  default = "subnet-b4d826ed"
 }
 variable "tag_name" {
   default = "test"
