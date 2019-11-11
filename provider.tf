3f294ff74408737723512b37b2f88bb506f5cffa
Dinesh Sharma <Dinesh.Sharma@ac3.com.au>
2019-11-11 13:42:14 +1300

    removed files

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "ap-southeast-2"
-    #account = "bppstraining"
-    profile = "bppstraining"
-    assume_role  {
-       role_arn= "arn:aws:iam::335908344139:role/bp-saml-rw"
-    }
-}
